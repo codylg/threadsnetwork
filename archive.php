d6658fd6da8bfff9a64617477c25c2c38562f8a3
Cody Gordon <clg@clg.name>
2018-04-19 20:10:03 +1000

    Generalise fancy post list template and add to archive pages

--- archive.php
+++ archive.php
@@ -23,18 +23,8 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
+			get_template_part( 'template-parts/fancy-post-list' );
 
 			the_posts_navigation();
 
