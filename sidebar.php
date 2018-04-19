7c465bca29e01e2030d7ea25849a0d901095e9b6
Cody Gordon <clg@clg.name>
2018-04-19 19:42:23 +1000

    Support sidebar widgets

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
-	This is the sidebar
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
